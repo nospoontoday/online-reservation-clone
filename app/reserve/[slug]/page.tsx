ad9fa97dcd0881e294472487a26eddf81de02fbc
Oliver <oliverjohnpr2013@gmail.com>
2023-08-20 10:49:27 +0800

    Manually switch from anchor tags to Link component

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,22 +1,24 @@
+import Link from "next/link";
+
 export default function Reserve() {
     return (
         <main className="bg-gray-100 min-h-screen w-screen">
             <div className="max-w-screen-2xl m-auto bg-white">
                 {/* NAVBAR */}
                 <nav className="bg-white p-2 flex justify-between">
-                <a href="" className="font-bold text-gray-700 text-2xl">
-                    {" "} OpenTable{" "}
-                </a>
-                <div>
-                    <div className="flex">
-                    <button
-                        className="bg-blue-400 text-white border p-1 px-4 rounded mr-3"
-                    >
-                        Sign in
-                    </button>
-                    <button className="border p-1 px-4 rounded">Sign up</button>
+                    <Link href="" className="font-bold text-gray-700 text-2xl">
+                        {" "} OpenTable{" "}
+                    </Link>
+                    <div>
+                        <div className="flex">
+                        <button
+                            className="bg-blue-400 text-white border p-1 px-4 rounded mr-3"
+                        >
+                            Sign in
+                        </button>
+                        <button className="border p-1 px-4 rounded">Sign up</button>
+                        </div>
                     </div>
-                </div>
                 </nav>
                 {/* NAVBAR END */}
                 <div className="border-t h-screen">
